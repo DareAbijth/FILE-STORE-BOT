0a3bfaa3ab17b3366ab1f9f87b4be685114dbfc7
αвιנтн <92922942+DareAbijth@users.noreply.github.com>
2021-12-10 17:52:40 +0530

    Update force_sub_handler.py

--- handlers/force_sub_handler.py
+++ handlers/force_sub_handler.py
@@ -47,7 +47,7 @@ async def handle_force_sub(bot: Client, cmd: Message):
     except Exception:
         await bot.send_message(
             chat_id=cmd.from_user.id,
-            text="Something went Wrong. Contact my [Support Group](https://t.me/moviesworld2ty).",
+            text="Something went Wrong. Contact my [Support Group](https://t.me/REX_Bots_Support).",
             parse_mode="markdown",
             disable_web_page_preview=True
         )
